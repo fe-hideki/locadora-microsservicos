ad4846b257b7ab1675e7493d5c2acec25f151d08
fe-hideki <felipe.dsouza@sptech.school>
2026-05-11 11:48:14 -0300

    first commit

---
+++ veiculo-service/src/test/java/com/example/veiculo_service/VeiculoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.veiculo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
